30dc40a267a0554a55d304b2a89aa37f0bd4bfa1
Vladimir Skok <skok@vova.io>
2021-01-31 15:59:31 -0500

    refactor: core functionality

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Git Sync
 
 GitHub ➡️ GitLab
+
+## Prerequisites
+
+Create GitHub [access token](https://github.com/settings/tokens/new) with `admin:public_key` and `repo` scope.
--- main.py
+++ main.py
@@ -1,23 +1,57 @@
 import os.path
 import json
 import subprocess
+import requests
 from github import Github
 from dotenv import load_dotenv
 
 load_dotenv()
 
 GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
-GITLAB_ACCESS_TOKEN = os.environ.get("GITLAB_ACCESS_TOKEN")
-REPOS_PATH = os.environ.get("REPOS_PATH")
 GITHUB_HANDLE = os.environ.get("GITHUB_HANDLE")
+GITHUB_KEY_TITLE = os.environ.get("GITHUB_KEY_TITLE")
+GITHUB_KEY_PATH = os.environ.get("GITHUB_KEY_PATH")
+GITHUB_API_URL = os.environ.get("GITHUB_API_URL")
+
+GITLAB_ACCESS_TOKEN = os.environ.get("GITLAB_ACCESS_TOKEN")
 GITLAB_HANDLE = os.environ.get("GITLAB_HANDLE")
 GITLAB_REMOTE = os.environ.get("GITLAB_REMOTE")
 GITLAB_URL = os.environ.get("GITLAB_URL")
+GITLAB_URL_PROTOCOL = os.environ.get("GITLAB_URL_PROTOCOL")
 GITLAB_SSH_PORT = os.environ.get("GITLAB_SSH_PORT")
 GITLAB_HTTP_PORT = os.environ.get("GITLAB_HTTP_PORT")
+
+REPO_BRANCHES = os.environ.get("REPO_BRANCHES")
+REPOS_PATH = os.environ.get("REPOS_PATH")
 STATE_PATH = os.environ.get("STATE_PATH")
 
 g = Github(GITHUB_ACCESS_TOKEN)
+
+def get_key_content():
+  try:
+    with open(GITHUB_KEY_PATH, "r", encoding="utf-8") as key:
+      return key.read()
+  except Exception as error:
+    print(error)
+
+def add_github_key():
+  response = requests.post(
+    f"{GITHUB_API_URL}/user/keys",
+    headers={
+      "Accept": "application/vnd.github.v3+json",
+      "Authorization": f"token {GITHUB_ACCESS_TOKEN}",
+    },
+    json={
+      "key": get_key_content(),
+      "title": GITHUB_KEY_TITLE
+    }
+  )
+
+  # Check if SSH key was created or has previously been registered.
+  return (
+    response.status_code == 201 or
+    response.status_code == 422
+  )
 
 def format_full_name(full_name):
   return full_name.replace("/", "-", 1)
@@ -41,65 +75,86 @@ def pull_repo(repo_path):
   )
 
 def create_project(repo_name):
-  try:
-    request = f'curl --header "PRIVATE-TOKEN: {GITLAB_ACCESS_TOKEN}" -X POST "http://{GITLAB_URL}:{GITLAB_HTTP_PORT}/api/v4/projects?name={repo_name}"'
-    subprocess.call(request, shell=True)
-  except Exception as error:
-    print("Gitlab create project API call failed.")
-    print(error)
+  response = requests.post(
+    f"{GITLAB_URL_PROTOCOL}://{GITLAB_URL}:{GITLAB_HTTP_PORT}/api/v4/projects?name={repo_name}",
+    headers={
+      "PRIVATE-TOKEN": GITLAB_ACCESS_TOKEN
+    }
+  )
+
+  if response.status_code == 201:
+    print(f"{repo_name} project was succesfully created in GitLab.")
+  elif response.status_code == 400:
+    print(f"{repo_name} project has already been registered in GitLab.")
+  else:
+    raise Exception(f"API call for creating {repo_name} project failed in GitLab.")
 
 def push_repo(repo_path):
-  branches = ["main", "master"]
+  branches = REPO_BRANCHES.split(",")
   check_branch = "git rev-parse --verify"
 
   for index, branch in enumerate(branches):
     try:
-      subprocess.check_output(f"cd {repo_path} && {check_branch} {branch}", shell=True)
-      subprocess.call(
-        f"cd {repo_path} && git push {GITLAB_REMOTE} {branch}",
-        shell=True
+      subprocess.check_output(
+        f"cd {repo_path} && {check_branch} {branch}",
+        stderr=subprocess.DEVNULL,
+        shell=True,
       )
-      print(f"Repo was successfully pushed in Gitlab. {repo_path}")
+      subprocess.call(
+        f"cd {repo_path} && git push --force {GITLAB_REMOTE} {branch}",
+        stderr=subprocess.DEVNULL,
+        shell=True,
+      )
+      print(f"{repo_path.replace(REPOS_PATH + '/', '')} was successfully pushed in Gitlab.")
       break
     except subprocess.CalledProcessError:
       if index == len(branches) - 1:
         print(f"Failed to push repo {repo_path}. Did not find default branch.")
 
-state = {}
+state = {
+  "is_github_key_added": False,
+  "repos": {}
+}
 
 if os.path.isfile(STATE_PATH):
   with open(STATE_PATH, "r", encoding="utf-8") as jsonFile:
     state = json.load(jsonFile)
 
-index = 0
+if state.get("is_github_key_added") != True:
+  if add_github_key():
+    state["is_github_key_added"] = True
+  else:
+    raise Exception("GitHub key was not added.")
+
+# index = 0
 for repo in g.get_user().get_repos():
-  index += 1
+  # index += 1
   full_name = format_full_name(repo.full_name)
   ssh_url = repo.ssh_url
   pushed_at = repo.pushed_at.timestamp()
   repo_path = os.path.join(REPOS_PATH, full_name)
 
-  if "gatsby" in repo_path:
-    continue
+  # if "gatsby" in repo_path:
+  #   continue
 
   if not os.path.isdir(repo_path):
     print("Repo was not cloned.")
     clone_repo(repo_path, ssh_url)
     create_project(full_name)
     add_remote(repo_path, full_name)
-  elif pushed_at > state[full_name]["updated"]:
+  elif pushed_at > state["repos"][full_name]["updated"]:
     print("Repo was updated since the last download.")
     pull_repo(repo_path)
 
   if full_name not in state:
-    state[full_name] = {}
+    state["repos"][full_name] = {}
 
-  state[full_name]["updated"] = pushed_at
+  state["repos"][full_name]["updated"] = pushed_at
 
   push_repo(repo_path)
 
-  if index == 5:
-    break;
+  # if index >= 20:
+  #   break;
 
 with open(STATE_PATH, "w", encoding="utf-8") as jsonFile:
   json.dump(state, jsonFile, indent=2)
