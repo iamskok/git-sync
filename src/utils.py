8338c14f21a8b30380bb6d151ceee344a5de84c0
Vladimir Skok <skok@vova.io>
2021-02-22 08:33:17 -0500

    fix: utility functions

--- src/utils.py
+++ src/utils.py
@@ -9,44 +9,55 @@ KNOWN_HOSTS_PATH = os.environ.get("KNOWN_HOSTS_PATH")
 REPOS_BLACKLIST = os.environ.get("REPOS_BLACKLIST")
 
 
+class InvalidSSHKeyError(Exception):
+    pass
+
+
 def get_ssh_key_content():
     if os.path.isfile(GITHUB_KEY_PATH):
         with open(GITHUB_KEY_PATH, "r", encoding="utf-8") as key:
             content = key.read()
 
-            is_valid_key = "is not a public key file." not in subprocess.check_output(
+            exit_code = subprocess.call(
                 f"ssh-keygen -l -f {GITHUB_KEY_PATH}",
                 shell=True
-            ).decode("utf-8")
+            )
 
-            if is_valid_key:
-                return content
+            if exit_code != 0:
+                raise InvalidSSHKeyError(
+                    f"Invalid SSH key. `ssh-keygen` returned non zero exit code - {exit_code}"
+                )
 
-            raise Exception(
-                f"{GITHUB_KEY_PATH} SSH public key has invalid format")
+            return content
     else:
-        raise Exception(f"{GITHUB_KEY_PATH} file path does not exist.")
+        raise FileNotFoundError(f"{GITHUB_KEY_PATH} file path does not exist.")
 
 
 def format_full_name(full_name):
     return full_name.replace("/", "-", 1)
 
 
+class InvalidHostError(Exception):
+    pass
+
+
 def add_known_host(host):
     # Handle custom port case
-    if len(host.split(":")) == 2:
-        _host = host.split(":")[0]
-        _port = host.split(":")[1]
-        subprocess.call(
-            f"ssh-keyscan -p {_port} -H {_host} >> {KNOWN_HOSTS_PATH}",
+    if host.find(":") > -1:
+        host, port, *_ = host.split(":")
+        exit_code = subprocess.call(
+            f"ssh-keyscan -p {port} -H {host} >> {KNOWN_HOSTS_PATH}",
             shell=True
         )
     else:
-        subprocess.call(
+        exit_code = subprocess.call(
             f"ssh-keyscan -H {host} >> {KNOWN_HOSTS_PATH}",
             shell=True
         )
 
+    if exit_code != 0:
+        raise InvalidHostError(f"Invalid host name {host}")
+
 
 def is_repo_blacklisted(repo, blacklist=REPOS_BLACKLIST):
-    return repo in blacklist.split(",")
+    return False if blacklist is None else repo in blacklist.split(",")
